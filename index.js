495566e3db061e0326f1fbca9eac433f835bb13b
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-16 23:14:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import {ApolloServer} from 'apollo-server'
-import {gql} from 'graphql-tag'
+const { ApolloServer} = require('apollo-server');
+const {gql}=require('graphql-tag')
+const {mongoose}=require('mongoose');
+mongoose.connect('mongodb+srv://sahilm:sahilm123@app-database.dfcxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true}).then(()=>{
+    console.log('MongoDB connected , launch fireworks!')
+})
 const typeDefs=gql`
 type Query{
      sayHi:String!
@@ -11,4 +15,11 @@ const resolvers={
     }
 }
 
-const server=new 
+const server=new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen({port:5000}).then(res=>{
+    console.log(`Server running at ${res.url}`)
+})
