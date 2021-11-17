21dd3ec23064ee2f8cf27d82a136b94b2918be9c
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-17 09:36:18 +0530

    Added post and user schemas

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { ApolloServer} = require('apollo-server');
 const {gql}=require('graphql-tag')
-const {mongoose}=require('mongoose');
-mongoose.connect('mongodb+srv://sahilm:sahilm123@app-database.dfcxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true}).then(()=>{
-    console.log('MongoDB connected , launch fireworks!')
-})
+const mongoose=require('mongoose');
 const typeDefs=gql`
 type Query{
      sayHi:String!
@@ -20,6 +17,10 @@ const server=new ApolloServer({
   resolvers
 })
 
-server.listen({port:5000}).then(res=>{
+mongoose.connect('mongodb+srv://sahilm:sahilm123@app-database.dfcxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true}).then(()=>{
+    console.log('MongoDB connected , launch fireworks!')
+    return server.listen({port:5000})
+})
+.then(res=>{
     console.log(`Server running at ${res.url}`)
 })
